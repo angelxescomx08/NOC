2e79c7e59047c1af799f3a3ffee53d1c5c564e33
angelxescomx08 <69997011+angelxescomx08@users.noreply.github.com>
2024-05-09 11:12:17 -0600

    use case

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,17 +1,10 @@
+import { CheckService } from "../domain/use-cases/checks/check-service";
 import { CronService } from "./cron/cron-service";
 
 export class Server {
   public static start() {
     CronService.createJob("*/5 * * * * *", () => {
-      console.log("Cada 5 segundos");
-    });
-
-    CronService.createJob("*/3 * * * * *", () => {
-      console.log("Cada 3 segundos");
-    });
-
-    CronService.createJob("*/2 * * * * *", () => {
-      console.log("Cada 2 segundos");
+      new CheckService().execute("https://google.com");
     });
   }
 }
